520f4e152f56a86d439578e446b79dac00572cc7
Kyle Buchmiller <kyle.buchmiller@gmail.com>
2025-10-08 10:39:45 -0400

    small updates to build

--- build.sh
+++ build.sh
@@ -1,15 +1,15 @@
 mkdir -p build && cd build
 
-# call "./build.sh prod" to disable debugging and debug macros in the code 
-if [ "$1" = "prod" ] ; then
-	cmake .. 
+# call "./build.sh release" to disable debugging and debug macros in the code 
+if [ "$1" = "release" ] ; then
+	cmake .. -DCMAKE_BUILD_TYPE=Release
 else
 	cmake .. -DCMAKE_BUILD_TYPE=Debug
 
 fi
 cmake --build .
 
-# quick reference example -- to add debug statements:
+# quick reference example -- to add debug statements in code:
 #ifdef ENG_DEBUG
     #std::cout << "debug is on! let's go\n";
 #endif
